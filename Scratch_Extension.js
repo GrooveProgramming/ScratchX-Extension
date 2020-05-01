c8a377bd83a884596b34d8d8fd024771f633d263
Spaceflyer234 <64615967+Spaceflyer234@users.noreply.github.com>
2020-05-01 07:38:31 -0500

    Update Scratch_Extension.js

--- Scratch_Extension.js
+++ Scratch_Extension.js
@@ -5,15 +5,16 @@
     // Status reporting code
     // Use this to report missing hardware, plugin or unsupported browser
     ext._getStatus = function() {
-        return {status: 2, msg: ‘Ready’};
+        return {status: 2, msg: 'Ready'};
     };
 
     // Block and block menu descriptions
     var descriptor = {
         blocks: [
-        ['b','loud?','loud']  
+            ['b','loud?','loud']
         ]
     };
+
     // Register the extension
     ScratchExtensions.register('Extension', descriptor, ext);
 })({});
